e59b7bd3f4a361c22193addae912d421079b6a04
ridemountainpig <lin0905580802@gmail.com>
2024-03-25 14:53:34 +0800

    docs: update readme

--- lib/updateSvglBadge.py
+++ lib/updateSvglBadge.py
@@ -52,11 +52,11 @@ def updateSvglBadgeReadme():
     readme = """
 # Svgl Badges
 
-[![Svgl](https://raw.githubusercontent.com/ridemountainpig/svgl-badge/main/public/badges/svgl.svg)](https://svgl.app)
-[![GitHub](https://raw.githubusercontent.com/ridemountainpig/svgl-badge/main/public/badges/github.svg)](https://github.com/ridemountainpig/svgl-badge)
-[![Vercel](https://raw.githubusercontent.com/ridemountainpig/svgl-badge/main/public/badges/vercel.svg)](https://vercel.com)
-[![Next.JS](https://raw.githubusercontent.com/ridemountainpig/svgl-badge/main/public/badges/nextjs.svg)](https://nextjs.org)
-[![Tailwind CSS](https://raw.githubusercontent.com/ridemountainpig/svgl-badge/main/public/badges/tailwindcss.svg)](https://tailwindcss.com)
+[![Svgl](https://svgl-badge.vercel.app/api/Library/Svgl?theme=light)](https://svgl.app)
+[![GitHub](https://svgl-badge.vercel.app/api/Software/Github?theme=light)](https://github.com/ridemountainpig/svgl-badge)
+[![Vercel](https://svgl-badge.vercel.app/api/Hosting/Vercel?theme=light)](https://vercel.com)
+[![Next.JS](https://svgl-badge.vercel.app/api/Framework/Next.js?theme=light)](https://nextjs.org)
+[![Tailwind CSS](https://svgl-badge.vercel.app/api/Framework/Tailwind%20CSS?theme=light)](https://tailwindcss.com)
 
 The Markdown Badges for all the SVGs available on [Svgl](https://svgl.app).
 
