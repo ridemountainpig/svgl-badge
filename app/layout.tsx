41066dcad9b3dd22b2d513efae59323c9f66a0ea
ridemountainpig <lin0905580802@gmail.com>
2024-04-11 10:15:31 +0800

    feat: add metadata and GA config

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,42 @@
 import type { Metadata } from "next";
+import { GoogleAnalytics } from "@next/third-parties/google";
 import "./globals.css";
 
 export const metadata: Metadata = {
     title: "Svgl Badges",
     description:
         "A beautiful badges with svgl SVG logos. Decorate your README.md profile with svgl badges",
+    keywords:
+        "svgl, svgl badges, svgl badge, svgl logo, svgl logos, svgl svg, svgl svg logos, svgl svg logo, svgl svg badges, svgl svg badge",
+    authors: [
+        {
+            name: "ridemountainpig",
+            url: "https://www.github.com/ridemountainpig",
+        },
+    ],
+    openGraph: {
+        type: "website",
+        url: "https://svgl-badge.vercel.app/",
+        title: "Svgl Badges",
+        description:
+            "A beautiful badges with svgl SVG logos. Decorate your README.md profile with svgl badges",
+        images: [
+            {
+                url: "https://svgl-badge.vercel.app/svgl.png",
+                width: 1200,
+                height: 630,
+                alt: "Svgl Badges",
+            },
+        ],
+    },
+    twitter: {
+        card: "summary_large_image",
+        title: "Svgl Badges",
+        description:
+            "A beautiful badges with svgl SVG logos. Decorate your README.md profile with svgl badges",
+        creator: "@ridemountainpig",
+        images: ["https://svgl-badge.vercel.app/svgl.png"],
+    },
 };
 
 export default function RootLayout({
@@ -15,6 +47,7 @@ export default function RootLayout({
     return (
         <html lang="en">
             <body>{children}</body>
+            <GoogleAnalytics gaId="G-SPCB204YQ0" />
         </html>
     );
 }
