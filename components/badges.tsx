f53127884bc9a0fc77317c61da59271dd8ca63fc
ridemountainpig <lin0905580802@gmail.com>
2024-04-15 01:08:08 +0800

    chore: update img attribute

--- components/badges.tsx
+++ components/badges.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import React from "react";
-import Image from "next/image";
 import toast from "react-hot-toast";
 
 interface BadgesProps {
@@ -13,14 +12,11 @@ export function Badges({ badges }: BadgesProps) {
         <div className="flex flex-wrap justify-center gap-[1.3rem] p-6">
             {Object.keys(badges).map((key, index) => {
                 const badge = badges[key];
-                const svgWidth = key.length * 10 + key.length * 0.4 + 52;
                 return (
                     <React.Fragment key={index}>
                         <img
                             src={badge.light}
                             alt={key}
-                            width={svgWidth}
-                            height={40}
                             className="block cursor-pointer transition-transform duration-300 hover:scale-115 dark:hidden"
                             onClick={() => {
                                 navigator.clipboard.writeText(badge.light);
@@ -31,8 +27,6 @@ export function Badges({ badges }: BadgesProps) {
                             <img
                                 src={badge.dark}
                                 alt={`${key}-dark`}
-                                width={svgWidth}
-                                height={40}
                                 className="hidden cursor-pointer transition-transform duration-300 hover:scale-115 dark:block"
                                 onClick={() => {
                                     navigator.clipboard.writeText(badge.dark);
